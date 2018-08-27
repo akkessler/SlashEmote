cf1c5e85bccd1dedeba9de0a5d9489bd44348bd1
Andy Kessler <akkessler@users.noreply.github.com>
2018-08-27 11:06:52 -0500

    Extract names to constants

--- model/model.py
+++ model/model.py
@@ -6,6 +6,10 @@ import io
 import sys
 import os
 
+BOT_NAME = 'SlashEmote'
+INTENT_NAME = 'EmoteIntent'
+SLOT_TYPE_NAME = 'Emotes'
+
 def export_resource(name, resourceType, exportType, version):
     response = lex.get_export(
         name=name,
@@ -36,15 +40,15 @@ lex = boto3.client('lex-models', 'us-east-1')
 
 cmd = sys.argv[1].lower()
 if cmd == 'import':
-    import_resource('SlashEmote','BOT')
-    import_resource('EmoteIntent','INTENT')
-    import_resource('Emotes','SLOT_TYPE')
+    import_resource(BOT_NAME,'BOT')
+    import_resource(INTENT_NAME,'INTENT')
+    import_resource(SLOT_TYPE_NAME,'SLOT_TYPE')
 elif cmd == 'export':
-    bots = lex.get_bot_versions(name='SlashEmote')['bots']
-    export_resource('SlashEmote','BOT','LEX', bots[-1]['version'])
-    intents = lex.get_intent_versions(name='EmoteIntent')['intents']
-    export_resource('EmoteIntent','INTENT','LEX', intents[-1]['version'])
-    slotTypes = lex.get_slot_type_versions(name='Emotes')['slotTypes']
-    export_resource('Emotes','SLOT_TYPE','LEX', slotTypes[-1]['version'])
+    bots = lex.get_bot_versions(name=BOT_NAME)['bots']
+    export_resource(BOT_NAME,'BOT','LEX', bots[-1]['version'])
+    intents = lex.get_intent_versions(name=INTENT_NAME)['intents']
+    export_resource(INTENT_NAME,'INTENT','LEX', intents[-1]['version'])
+    slotTypes = lex.get_slot_type_versions(name=SLOT_TYPE_NAME)['slotTypes']
+    export_resource(SLOT_TYPE_NAME,'SLOT_TYPE','LEX', slotTypes[-1]['version'])
 else:
     print("Usage: {0} (import/export)", file=sys.stderr)
